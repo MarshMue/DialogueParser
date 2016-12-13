fb55cad47aa678feca3056fd24aca9355139d580
marshallmueller <mashymue@gmail.com>
2016-12-13 16:49:13 -0600

    Working with speech recognition

--- parser.py
+++ parser.py
@@ -2,11 +2,16 @@
 
 import nltk
 import json
-
+import speech_recognition as sr
 
 
 
 if __name__ == "__main__":
+
+    # set up speech recognition
+    r = sr.Recognizer()
+    m = sr.Microphone()
+
     # load in data
     with open("json/objects.json") as objectFile:
         objects = json.load(objectFile)
@@ -22,10 +27,27 @@ if __name__ == "__main__":
 
     while(q):
         # take in user string
-        text = raw_input("enter a task: ")
-        if text == "q":
-            q = False
-        tokens = nltk.word_tokenize(text)
+
+        print("Say something!")
+        with m as source:
+            audio = r.listen(source)
+        print("Got it! Now to recognize it...")
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
+
+            # we need some special handling here to correctly print unicode characters to standard output
+            if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+                print(u"You said {}".format(value).encode("utf-8"))
+                if format(value).encode("utf-8") == "quit":
+                    q = False
+            else:  # this version of Python uses unicode for strings (Python 3+)
+                print("You said {}".format(value))
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+        tokens = nltk.word_tokenize(format(value).encode("utf-8"))
 
         # find the action and object
         for token in tokens:
