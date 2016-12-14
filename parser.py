04a244962e624dc0369fab8eca42905b5668bca2
marshallmueller <mashymue@gmail.com>
2016-12-14 16:41:03 -0600

    updated with ros abilities

--- parser.py
+++ parser.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 
+import os
 import nltk
 import json
 import speech_recognition as sr
-
+import xml.etree.ElementTree as ET
+import rospy
+from std_msgs.msg import String
 
 
 if __name__ == "__main__":
 
+    # ROS init
+    pub = rospy.Publisher('mav_task', String, queue_size=10)
+    rospy.init_node('control_node', anonymous=True)
+    rospy.sleep(1)
+
     # set up speech recognition
     r = sr.Recognizer()
     m = sr.Microphone()
+    
+    #data 
+    objects = []
 
+    dr = "../models/"
     # load in data
-    with open("json/objects.json") as objectFile:
-        objects = json.load(objectFile)
+    for directory in os.listdir(dr):
+        if directory[0] != ".": 
+            tree = ET.parse(dr + directory + "/model.config")
+            root = tree.getroot()
+            mname = root[0].text
+            print type(mname)
+            objects.append(mname)
+
+    #with open("json/objects.json") as objectFile:
+     #   objects = json.load(objectFile)
 
     with open("json/actions.json") as actionFile:
         actions = json.load(actionFile)
@@ -45,6 +65,7 @@ if __name__ == "__main__":
                 print("You said {}".format(value))
         except sr.UnknownValueError:
             print("Oops! Didn't catch that")
+            continue
         except sr.RequestError as e:
             print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
         tokens = nltk.word_tokenize(format(value).encode("utf-8"))
@@ -52,17 +73,19 @@ if __name__ == "__main__":
         # find the action and object
         for token in tokens:
             for key in actions:
-                if token.lower() == key:
+                if token.lower() == key.lower():
                     action = actions[key]
             for item in objects:
-                if item == token.lower():
+                if item.lower() == token.lower():
                     object = item
 
-        # assume move if no action specified
-        action = "move"
 
         # tags = nltk.pos_tag(tokens)
         print "action: " + str(action) + " object: " + str(object)
+
+        # publish ros data
+        if action is not None and object is not None:
+            ros.publish(object)
 
         # reset data
         action = None
